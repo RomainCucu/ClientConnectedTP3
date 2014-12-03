cccf5be2b380097775e46fee984771cf45f43fa4
romaincucu <romain.cunault@gmail.com>
2014-12-03 22:29:55 +0100

    j

--- client2.js
+++ client2.js
@@ -15,7 +15,7 @@ var client = net.connect({port: 1337}, function () {
         if (err) throw err;
         if(stats.size>=100){//if size of the file is superior thant 100 octet
           var rest = stats.size%100;//to send data 100 byte per 100 byte then to send the rest
-          var result = stats.size-rest;
+          var result = stats.size-rest;// if size = 1260, we do 100 by 100 then 1 by 1
           var globalSize = stats.size;
           var i = 0;
           sendData();
@@ -25,14 +25,19 @@ var client = net.connect({port: 1337}, function () {
                 console.log(err);
               });
               fileStream.on('open',function(chunk){
-                fileStream.pipe(client);
+                fileStream.pipe(client,{ end: false });
               });
               fileStream.on('end',function(chunk){
                 console.log('bite from '+i+' to '+(i+99)+' sent');
-                if(i<result){                
+                if(i<result-100){                
                   i+=100;
-                  setTimeout(sendData(),1000);
-                }else return;
+                  setTimeout(sendData,100);
+                }else if(i-(result-100)<=rest){
+                  console.log('bite from '+i+' to '+(i+99)+' sent');
+                  i++;
+                  setTimeout(sendData,100);
+                }
+                else return;
               });              
             }           
           }//if stats.size        
@@ -45,7 +50,7 @@ var client = net.connect({port: 1337}, function () {
 });
 
 client.on('data', function(data) {
-  console.log(''+data);
+  //console.log(''+data);
 });
 /**
 client receive end event from server
