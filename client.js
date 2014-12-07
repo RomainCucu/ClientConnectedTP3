376bf0d8c4f8cb50e53e006815a84ef8e838a871
romaincucu <romain.cunault@gmail.com>
2014-12-07 15:34:15 +0100

    l

--- client.js
+++ client.js
@@ -19,45 +19,13 @@ If file exists, "exists" is true, else is false
    	/**
     stats is an object with a field called "size" which is equal to file size (eg:21 octets)
     */  
-    var stream = fs.createReadStream(fileName, { bufferSize: 64 * 1024 });
-    stream.pipe(client);  	
-      /*
-      fs.open(fileName, "r", function(error, fd) {
-      	/**
-      	fd is a file descriptor, dont know what does that mean
-      	
-        /*var buffer = new Buffer(stats.size); 		
-        fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer) {
-          var data = buffer.toString("utf8", 0, buffer.length);
-          client.write(data);
-          fs.close(fd);
-        });
-    */
-      //});
+      var stream = fs.createReadStream(fileName, { bufferSize: 64 * 1024 });
+      stream.pipe(client);
     });
   }else{
   	console.log("fileNotFound");
   }
 });
-
-	/*//client.write("un client se connecte");
-	var fileStream = fs.createReadStream(fileName);
-
-    fileStream.on('error', function(err){
-        console.log(err);
-    });
-
-    fileStream.on('open',function(chunk){
-    	//console.log('got %d bytes of data', chunk.length);
-    	fileStream.pipe(client);
-    	//console.log(chunk);
-    	//client.write(chunk);
-    });
-
-    fileStream.on('end', function () {
-  		console.log('got all bytes of data');
-	});
-*/
 });
 
 client.on('data', function(data) {
--- server.js
+++ server.js
@@ -9,15 +9,21 @@ var server = net.createServer(function (socket){
   console.log('server connected');
 
   socket.write('hello\r\n');
-  socket.pipe(socket); 
-
+  //socket.pipe(socket);     
+    var b = "";
 	  socket.on('data',function(chunk){	  	
 	  	console.log(""+chunk);
+      b += chunk;
 	  });
 
     socket.on('end', function() {
     	console.log('client disconnected');
-  });
+      console.log("saved");
+      fs.writeFile('message.txt', b, function (err) {
+        if (err) throw err;
+        console.log('It\'s saved!');
+      });//write
+  });//socket on
 });
 /**
 let's listening the server
